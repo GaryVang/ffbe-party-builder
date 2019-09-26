3dabde25a67585fb9854850786b0ccab02b4d70a
MysticV <leng_vang89@hotmail.com>
2019-09-25 21:34:56 -0700

    Added elemental resistance panel, changed element and ailment font-sizes to vw, changed unit-stat font-size to vh for ipads, changed css for unit-image (needs to be redone for resizing and centering

--- src/components/UnitInfo/UnitInfo.js
+++ src/components/UnitInfo/UnitInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './UnitInfo.css';
-import lightning from './unit-lightning.png';
+import imgLightning from './unit-lightning.png';
 import poison from './AilmentIcon/ailment-poison.png';
 import blind from './AilmentIcon/ailment-blind.png';
 import sleep from './AilmentIcon/ailment-sleep.png';
@@ -9,6 +9,15 @@ import paralysis from './AilmentIcon/ailment-paralysis.png';
 import confusion from './AilmentIcon/ailment-confusion.png';
 import disease from './AilmentIcon/ailment-disease.png';
 import petrification from './AilmentIcon/ailment-petrification.png';
+
+import fire from './ElementalIcon/element-fire.png';
+import ice from './ElementalIcon/element-ice.png';
+import lightning from './ElementalIcon/element-lightning.png';
+import water from './ElementalIcon/element-water.png';
+import wind from './ElementalIcon/element-wind.png';
+import earth from './ElementalIcon/element-earth.png';
+import light from './ElementalIcon/element-light.png';
+import dark from './ElementalIcon/element-dark.png';
 
 const UnitInfo = ({ name= "Lightning", stats }) => {
     return (
@@ -31,7 +40,7 @@ const UnitInfo = ({ name= "Lightning", stats }) => {
                 <ul><b>SPR: 1000</b></ul>
             </div>
             <div className='unit-img'>
-                <img alt='unit img' src={lightning} />
+                <img alt='unit img' src={imgLightning} />
             </div>
             <div className='unit-weapon-type'>
                 <ul><b>TDW: 100% ATK: +100</b></ul>
@@ -47,7 +56,7 @@ const UnitInfo = ({ name= "Lightning", stats }) => {
                 <img className='icon-confusion' alt='ailment' src={confusion} />
                 <img className='icon-disease' alt='ailment' src={disease} />
                 <img className='icon-petrification' alt='ailment' src={petrification} />
-            	<div className='test1'>100</div>
+            	<div>100</div>
             	<div>100</div>
             	<div>100</div>
             	<div>100</div>
@@ -56,7 +65,24 @@ const UnitInfo = ({ name= "Lightning", stats }) => {
             	<div>100</div>
             	<div>100</div>
             </div>
-            <div className='unit-element'>Element</div>
+            <div className='unit-element'>
+                <img className='icon-fire' alt='ailment' src={fire} />
+                <img className='icon-ice' alt='ailment' src={ice} />
+                <img className='icon-lightning' alt='ailment' src={lightning} />
+                <img className='icon-water' alt='ailment' src={water} />
+                <img className='icon-wind' alt='ailment' src={wind} />
+                <img className='icon-earth' alt='ailment' src={earth} />
+                <img className='icon-light' alt='ailment' src={light} />
+                <img className='icon-dark' alt='ailment' src={dark} />
+            	<div>100</div>
+            	<div>100</div>
+            	<div>100</div>
+            	<div>100</div>
+            	<div>100</div>
+            	<div>100</div>
+            	<div>100</div>
+            	<div>100</div>   
+            </div>
         </div>
     );
 }
