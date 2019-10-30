18c6a21b33f64602bb3da16f4a4fb109fce134ba
MysticV <leng_vang89@hotmail.com>
2019-10-30 02:07:25 -0700

    Working search ordered list

--- src/components/UnitSearch/UnitSearch.js
+++ src/components/UnitSearch/UnitSearch.js
@@ -37,46 +37,104 @@ class UnitSearch extends React.Component {
   //   )
   // }
 
+  // createList = ({ items }) => {
+  //   console.log('SearchList: ', items);
+  //   return (
+  //     <ul>
+  //       {
+  //         items.map((item, i) => <li>{item}</li>) 
+  //       }
+  //     </ul>
+  //   )
+  // } 
+
   filterUnitList = () => {
     const { unitList } = this.props;
     const { filteredList, value } = this.state;
-    console.log('opening value: ', value);
-    filteredList.length = 0; //Resets list
-    // console.log('inside filter');
+    console.log('1 opening value: ', value);
+    // filteredList.length = 0; 
+    this.setState({
+      filteredList: []
+    }, function() {
+      
+      let tmpArray = [];
 
-    if(value !== '')
-    {
-      for (var key in unitList) {
-        // skip loop if the property is from prototype
-        if (!unitList.hasOwnProperty(key)) continue;
-    
-        var obj = unitList[key];
-        for (var prop in obj) {
-            // skip loop if the property is from prototype
-            if (!obj.hasOwnProperty(prop)) continue;
-    
-              if(prop === 'name') //------Change to check for textfield input
-              {
-                // console.log('Name: ', obj[prop]);
-                if(obj[prop].toLowerCase().includes(value.toLowerCase()))
+      if(value !== '')
+      {
+        
+        for (var key in unitList) {
+          // skip loop if the property is from prototype
+          if (!unitList.hasOwnProperty(key)) continue;
+          
+          var obj = unitList[key];
+          for (var prop in obj) {
+              // skip loop if the property is from prototype
+              if (!obj.hasOwnProperty(prop)) continue;
+              
+                if(prop === 'name') //------Change to check for textfield input
                 {
-                  filteredList.push(obj[prop]);
+                  // console.log('Name: ', obj[prop]);
+                  
+                  if(obj[prop].toLowerCase().includes(value.toLowerCase()))
+                  {
+                    console.log('1 obj', obj[prop]);
+                    // filteredList.push(obj[prop]);
+                    // console.log('type: ', obj[prop]);
+
+                    //Doesn't rely on using prop to directly modify state
+                    //Works
+                    tmpArray.push(obj[prop]); 
+                    this.setState({
+                      filteredList: tmpArray
+                    })
+  
+                    //closest to working
+                    // this.setState({
+                    //   filteredList: [...this.state.filteredList, obj[prop]]
+                    // }, function(){
+                    //   console.log('2 obj:', obj[prop]);
+                    //   console.log('3 obj:', obj[prop]);
+                    //   console.log('push: ', this.state.filteredList);
+                    // })
+
+                    // this.setState( prevState=> ({
+                    //   filteredList: [...this.state.filteredList, obj[prop]]
+                    // }, function(){
+                    //   console.log('push: ', this.state.filteredList);
+                    // }));
+
+                    // this.setState(state => {
+                    //   const filteredList = state.filteredList.concat(state.obj[prop]);
+
+                    //   return {
+                    //     filteredList,
+                        
+                    //   };
+                    // });
+
+                  }
+                    // console.log('inside if: ', filteredList);
+                    // console.log('obj[prop]: ', obj[prop]);
+                    // console.log('value: ', value);
+                    // console.log('typeof obj[prop]: ', typeof obj[prop]);
+                    // console.log('typeof value: ', value);
                 }
-                  // console.log('inside if: ', filteredList);
-                  // console.log('obj[prop]: ', obj[prop]);
-                  // console.log('value: ', value);
-                  // console.log('typeof obj[prop]: ', typeof obj[prop]);
-                  // console.log('typeof value: ', value);
-              }
-                // alert(obj[prop]);
+                  // alert(obj[prop]);
+          }
         }
       }
-    }
+
+    });
+    // console.log('inside filter');
+    // console.log('unitList prop: ', unitList);
+
+
     console.log('filterUnitList: ', filteredList);
+    // createList(filteredList);
   };
 
   handleChange(event) {
-    console.log('backspace');
+    console.log('input changed');
     // console.log('handleChange 1 value: ', this.state.value);
     // this.setState({value: event.target.value});
     // this.setState({value: event.target.value}, this.filterUnitList());
@@ -85,7 +143,7 @@ class UnitSearch extends React.Component {
       this.filterUnitList();
       // SearchList(this.state.filteredList);
     });
-    console.log('handleChange 2 value: ', this.state.value);
+    // console.log('input changed end: ', this.state.value);
     // console.log('handleChange eventvalue: ', event.target.value);
 
     // this.filterUnitList();
@@ -102,7 +160,7 @@ class UnitSearch extends React.Component {
             type="text"
             placeholder="Ex: Olive"
             // onKeyUp={filterUnitList()}
-            value = {this.state.value}
+            // value = {this.state.value} //Specifies initial value
             onChange={this.handleChange}
             
             
@@ -116,6 +174,14 @@ class UnitSearch extends React.Component {
 }
 
 export default UnitSearch;
+
+{/* <ul>
+{this.state.list.map(item => (
+  <li key={item}>{item}</li>
+))}
+</ul> */}
+
+
 
 // for (var key in unitList) {
 //   // skip loop if the property is from prototype
